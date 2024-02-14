1f8fb14d93c140e98dec70c4afcecbb203046af1
Fridakea <143487024+Fridakea@users.noreply.github.com>
2024-02-14 14:18:16 +0100

    Excample on fetch from our API

---
+++ main.js
@@ -0,0 +1,29 @@
+// Fetch data om ingredienser
+fetch("https://ayhgznyvoxhuiwpetdcp.supabase.co/rest/v1/ingredient", {
+    method: "GET",
+    headers: {
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5aGd6bnl2b3hodWl3cGV0ZGNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4NDE2NTcsImV4cCI6MjAyMzQxNzY1N30.8sKL9HgJTYSl6pgiZbRHk8qLBjPg9ebrnH6VQft3nx0"
+    }
+})
+    .then(response => response.json())
+    .then(showExampleIngredient)
+
+function showExampleIngredient(dataExample) {
+    console.log("Ingredients");
+    console.table(dataExample);
+}
+
+// Fetch data om opskrifter
+fetch("https://ayhgznyvoxhuiwpetdcp.supabase.co/rest/v1/recipe", {
+    method: "GET",
+    headers: {
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5aGd6bnl2b3hodWl3cGV0ZGNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4NDE2NTcsImV4cCI6MjAyMzQxNzY1N30.8sKL9HgJTYSl6pgiZbRHk8qLBjPg9ebrnH6VQft3nx0"
+    }
+})
+    .then(response => response.json())
+    .then(showExampleRecipe)
+
+function showExampleRecipe(dataExample) {
+    console.log("Recepies");
+    console.table(dataExample);
+}
