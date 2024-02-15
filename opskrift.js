e1181c057154a7cdcafa276e37f5f23bb66776c8
Rose-Marie <rora0001@stud.kea.dk>
2024-02-15 16:07:42 +0100

    dataen er inde

--- opskrift.js
+++ opskrift.js
@@ -13,25 +13,22 @@ fetch("https://ayhgznyvoxhuiwpetdcp.supabase.co/rest/v1/recipe", {
   .then((response) => response.json())
   .then(showRecipe)
   .catch((error) => console.error("Fejl ved indlæsning af opskrifter:", error));
-
 function showRecipe(data) {
   console.log("Recipe");
   console.log(data);
 
-
   // Vis kun data, hvis der er mindst én opskrift
   if (data.length > 0) {
-    const opskrift = data[0];
-    console.log(opskrift);
+    // Iterer gennem hvert element i arrayet
+    data.forEach((opskrift) => {
+      // Klon templaten til at vise opskrifter
+      let opskriftKlon = opskriftTemplate.cloneNode(true).content;
 
-    // Klon templaten til at vise opskrifter
-    let opskriftKlon = opskriftTemplate.cloneNode(true).content;
-    console.log(opskriftKlon);
+      opskriftKlon.querySelector(".opskrift_image").src = `images-recepies/${opskrift.image}`;
+      opskriftKlon.querySelector(".opskrift_name").textContent = `Navn: ${opskrift.name}`;
+      opskriftKlon.querySelector(".opskrift_seasons").textContent = `Sæsoner: ${opskrift.seasons}`;
 
-    opskriftKlon.querySelector(".opskrift_image").src = `images-recepies/${opskrift.image}`;
-    opskriftKlon.querySelector(".opskrift_name").textContent = `Navn: ${opskrift.name}`;
-    opskriftKlon.querySelector(".opskrift_seasons").textContent = `Sæsoner: ${opskrift.seasons}`;
-
-    opskriftsContainer.appendChild(opskriftKlon);
+      opskriftsContainer.appendChild(opskriftKlon);
+    });
   }
 }
