79385a9b3bdb3ec4b82d2fcc1963e3a3a666205d
Fridakea <frbe0002@stud.kea.dk>
2024-02-18 17:49:57 +0100

    Made ingredients using js

--- ingredient.js
+++ ingredient.js
@@ -1,7 +1,9 @@
+window.addEventListener("DOMContentLoaded", init);
+
 // Konstanter og variabler
-const ingredientTemplate = document.querySelector(".ingredient-template");
 const ingrediensContainer = document.querySelector(".ingredients-overview-container");
 
+function init() {
 // Fetch data om ingredienser
 fetch("https://ayhgznyvoxhuiwpetdcp.supabase.co/rest/v1/ingredient", {
   method: "GET",
@@ -11,9 +13,22 @@ fetch("https://ayhgznyvoxhuiwpetdcp.supabase.co/rest/v1/ingredient", {
 })
   .then((response) => response.json())
   .then(showIngredient);
+}
 
 function showIngredient(data) {
   console.log(data);
 
-  document.querySelector(".ingredient img").src = `images/${data.image}`
+  data.forEach(function(ingredientData) {
+    const template = document.querySelector("#ingredient-template");
+    const ingredientClone = template.cloneNode(true).content;
+    console.log(ingredientClone);
+
+    ingredientClone.querySelector(".ingredient img").src = `images/${ingredientData.image}`
+    ingredientClone.querySelector(".ingredient img").alt = `images of ${ingredientData.image}`
+    ingredientClone.querySelector(".ingredient h3").textContent = ingredientData.name
+    ingredientClone.querySelector(".ingredient").href = `opskrift_oversigt.html?=${ingredientData.ingredients}`
+
+    // Insert into the actual DOM 
+    document.querySelector(".ingredients-overview-container").appendChild(ingredientClone);
+  })
 }
