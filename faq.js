f8a354f8326aea947c8d227abe33b3d2612dc82e
jhessicacarvalho <132554850+jhessicacarvalho@users.noreply.github.com>
2024-02-20 14:02:47 +0100

    nye

--- faq.js
+++ faq.js
@@ -1,20 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
     const boxes = document.querySelectorAll('.box');
+    const overlay = document.querySelector('.overlay');
 
     boxes.forEach(box => {
-        box.addEventListener('click', function () {
-            boxes.forEach(b => {
-                b.classList.remove('active');
-            });
-            this.classList.add('active');
+        box.addEventListener('click', function (event) {
+            event.stopPropagation(); // Forhindrer klikhændelser på boksen i at sprede sig til parent-elementer
+            toggleOverlay();
         });
     });
 
+    // Luk overlay, når der klikkes uden for boksen eller overlayet
     document.addEventListener('click', function (event) {
-        if (!event.target.closest('.box')) {
-            boxes.forEach(box => {
-                box.classList.remove('active');
-            });
+        if (!event.target.closest('.box') && !event.target.closest('.overlay-content')) {
+            closeOverlay();
         }
     });
+
+    function toggleOverlay() {
+        overlay.classList.toggle('active'); // Skifter mellem tilføjelse og fjernelse af 'active' klasse på overlay
+    }
+
+    function closeOverlay() {
+        overlay.classList.remove('active'); // Fjerner 'active' klasse fra overlay
+    }
 });
